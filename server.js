76e4162be4f925e1ae35314f129c39a5c61b7d51
Daniel <ardekanicapital@gmail.com>
2020-03-28 22:51:56 -0400

    Practice marking up new webpage with pug syntax

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    name: 'Daniel Ardekani'
+  });
 });
 
 const server = app.listen(7000, () => {
