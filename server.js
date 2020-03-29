441116b4877dd027e205dce466f92c9dc39c1f7e
Daniel <ardekanicapital@gmail.com>
2020-03-29 15:53:47 -0400

    Create new profile view, create twitter button, and adjust profile picture width

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 let express = require('express');
 let people = require('./people.json');
-
 let app = express();
 
 app.set('view engine', 'pug');
-
 app.use(express.static(__dirname + '/public'));
 
+//homepage route//
 app.get('/', (req, res) => {
   res.render('index', {
     title: 'Homepage',
@@ -14,6 +13,16 @@ app.get('/', (req, res) => {
   });
 });
 
+//profile route//
+app.get('/profile', (req, res) => {
+  let person = people.profiles.find(p => p.id === req.query.id);
+  res.render('profile', {
+    title: `About ${person.firstname} ${person.lastname}`,
+    person,
+  });
+});
+
+//server message in iterm//
 let server = app.listen(7000, () => {
   console.log(`Running port ${server.address().port}`);
 });
