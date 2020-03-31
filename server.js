cd0cef2798d74bc931f1d73c6b1b5f6e0989f9a1
Daniel <ardekanicapital@gmail.com>
2020-03-30 22:28:50 -0400

    clean up syntax, add resume download route

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.render('index', {
     title: 'Homepage',
-    people: people.profiles
+    people: people.profiles,
   });
 });
 
@@ -18,7 +18,7 @@ app.get('/profile', (req, res) => {
   let person = people.profiles.find(p => p.id === req.query.id);
   res.render('profile', {
     title: `About ${person.firstname} ${person.lastname}`,
-    person,
+    person
   });
 });
 
